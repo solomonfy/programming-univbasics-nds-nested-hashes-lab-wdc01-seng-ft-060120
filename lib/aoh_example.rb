4168bd2c36c9f14eaaa541d83710d570afb87dbe
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-27 22:47:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,10 +20,13 @@ def literal_aoh
   
   #[DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G
   
-  [{name:  "Don Gately", occupation: "Live-in Staff" }, {name:  "Joelle van Dyne", occupation: "Radio Personality" }, {name:  "Pat Monteseian", occupation: "Staff" }, {name:  "Kate Gompert", occupation: "None" }, {name:  "Bruce Green", occupation: "Fan of Mildred" }
-  
+  [
+    {name:  "Don Gately", occupation: "Live-in Staff" },
+    {name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    {name:  "Pat Monteseian", occupation: "Staff" },
+    {name:  "Kate Gompert", occupation: "None" },
+    {name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
-  
 end
 
 def aoh_lookup(aoh, row, key)
